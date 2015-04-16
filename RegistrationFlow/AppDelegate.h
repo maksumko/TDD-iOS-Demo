6770204a92d6f177b4f98005edc519bfe4962417
maxim.k <mkos@ciklum.com>
2015-04-16 19:38:25 +0300

    Initial Commit

---
+++ RegistrationFlow/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RegistrationFlow
+//
+//  Created by maxim.k on 4/16/15.
+//  Copyright (c) 2015 maxim.k. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
